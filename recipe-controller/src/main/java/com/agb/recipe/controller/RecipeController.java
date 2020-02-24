04c1904ba64b58353fe3b08f8a533fc52e4fde45
Angela <agbenway@gmail.com>
2020-02-23 18:19:23 -0600

    Add Pageable RecipeSerch functionality

--- recipe-controller/src/main/java/com/agb/recipe/controller/RecipeController.java
+++ recipe-controller/src/main/java/com/agb/recipe/controller/RecipeController.java
@@ -4,15 +4,19 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agb.recipe.controller.converter.RecipeDtoToRecipeConverter;
 import com.agb.recipe.controller.domain.Recipe;
 import com.agb.recipe.storage.domain.RecipeDto;
+import com.agb.recipe.storage.domain.RecipeSearchRequest;
 import com.agb.recipe.storage.exception.DuplicateRecipeException;
 import com.agb.recipe.storage.exception.RecipeNotFoundException;
 import com.agb.recipe.storage.service.RecipeService;
@@ -26,22 +30,36 @@ public class RecipeController
     @Autowired
     private ConversionService recipeConverter;
 
-    // TODO: Search recipes (Pageable)
-
-    @RequestMapping(value = "/recipe", method = RequestMethod.POST)
-    public void createRecipe (@Valid @RequestBody Recipe recipe) throws RecipeNotFoundException, DuplicateRecipeException
+    @RequestMapping(value = "/recipes", method = RequestMethod.POST)
+    public @ResponseBody Recipe createRecipe (@Valid @RequestBody Recipe recipe) throws RecipeNotFoundException, DuplicateRecipeException
     {
         RecipeDto recipeDto = recipeConverter.convert(recipe, RecipeDto.class);
-        recipeService.createRecipe(recipeDto);
+
+        recipeDto = recipeService.createRecipe(recipeDto);
+
+        Recipe retval = recipeConverter.convert(recipeDto, Recipe.class);
+        return retval;
     }
 
-    @RequestMapping(value = "/recipe", method = RequestMethod.PUT)
-    public void updateRecipe (@Valid @RequestBody Recipe recipe)
+    @RequestMapping(value = "/recipes", method = RequestMethod.PUT)
+    public void updateRecipeFeedback (@Valid @RequestBody Recipe recipe)
     {
-        // recipeService.updateRecipe(recipe);
+        // recipeService.updateRecipeFeedback(recipe);
     }
 
-    @RequestMapping(value = "/recipe/{recipeId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/recipes", method = RequestMethod.GET)
+    public Page<Recipe> retrieveAllRecipes (@RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size)
+    {
+        RecipeSearchRequest request = new RecipeSearchRequest(page, size);
+
+        Page<RecipeDto> recipeDto = recipeService.retrieveRecipes(request);
+        Page<Recipe> recipes = recipeDto.map(new RecipeDtoToRecipeConverter()::convert);
+
+        return recipes;
+    }
+
+    @RequestMapping(value = "/recipes/{recipeId}", method = RequestMethod.GET)
     public @ResponseBody Recipe retrieveRecipe (@PathVariable(value = "recipeId") Long recipeId) throws RecipeNotFoundException
     {
         RecipeDto recipeDto = recipeService.retrieveRecipe(recipeId);
